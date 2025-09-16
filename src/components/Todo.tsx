18be04a4df7fe0fd2017d458437ef52bcf7df5a7
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-09-16 11:01:40 +0530

    typescript change

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 const Todo = () => {
   const [todoItems, setTodoItems] = useState<string[]>(() => {
